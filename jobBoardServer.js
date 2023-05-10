9f3298fd0f9efcd5893cf06f07d0e912fac322fe
Griffin Anderson <griffinanderson@griffins-air.mynetworksettings.com>
2023-05-09 20:51:41 -0400

    Base commit

--- jobBoardServer.js
+++ jobBoardServer.js
@@ -8,3 +8,17 @@ const app = express();
 const portNumber = process.argv[2];
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config({ path: path.resolve(__dirname, '.env') });
+
+const username = process.env.MONGO_DB_USERNAME;
+const password = process.env.MONGO_DB_PASSWORD;
+const database = process.env.MONGO_DB_NAME;
+const boardCollection = process.env.MONGO_BOARD_COLLECTION;
+const appCollection = process.env.MONGO_APP_COLLECTION;
+
+app.set('views', path.resolve(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+
+app.listen(portNumber);
+app.get('/', (request, response) => {
+  response.render('index');
+});
